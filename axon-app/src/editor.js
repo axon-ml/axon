6cf4133a24335493d4513090669e0419f8347cc8
Andrew Duffy <andreweduffy@gmail.com>
2017-04-15 17:30:04 -0700

    remove extra print

--- axon-app/src/editor.js
+++ axon-app/src/editor.js
@@ -15,7 +15,6 @@ export const EditorSettings = {
 export function load(elem, settings) {
     console.debug(`Loading editor @ ${elem}`);
     let stringRep = JSON.stringify(settings);
-    console.log(stringRep);
     eval(`
         require.config({ paths: { 'vs': 'node_modules/monaco-editor/min/vs/' }});
         require(['vs/editor/editor.main'], function() {
