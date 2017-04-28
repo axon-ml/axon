fb4ed042fded63fc91e8d1e8abedf82874a002ab
Andrew Duffy <andreweduffy@gmail.com>
2017-04-28 13:29:21 -0700

    initial work on db models

--- backend/schema/create.sql
+++ backend/schema/create.sql
@@ -1,7 +1,28 @@
-drop table if exists users; 
-create table users (
-	userId text primary key, 
-	numUnits integer
-); 
+-- Users table
+CREATE TABLE IF NOT EXISTS users (
+  id INT PRIMARY KEY,
+  name VARCHAR,
+  handle VARCHAR NOT NULL UNIQUE,
 
-insert into users values("tim", "23");
+  -- Only store salted+hashed passwords
+  pass_hash = STRING NOT NULL,
+  pass_salt STRING NOT NULL
+);
+-- Models table
+CREATE TABLE IF NOT EXISTS models (
+  id INT PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  owner INT NOT NULL FOREIGN Key (users.id)
+
+  -- model_id for parent model that this model is forked from.
+  -- Can be NULL if it is not a fork.
+  parent INT FOREIGN KEY (models.id),
+
+  -- A user can only have one repository with a given name
+  UNIQUE (owner, name)
+);
+-- Table of stars
+CREATE TABLE IF NOT EXISTS stars (
+    user INT FOREIGN KEY (users.id),
+    model INT FOREIGN KEY (models.id)
+);
