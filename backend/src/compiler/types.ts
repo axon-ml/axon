0a9175e6f40361fac952f66c4722abd165b4daef
Andrew Duffy <andreweduffy@gmail.com>
2017-05-08 00:27:46 -0700

    re-organized types module for compiler, added TypeAssertions

--- backend/src/compiler/types.ts
+++ backend/src/compiler/types.ts
@@ -1,3 +1,79 @@
+/**
+ * Typed representations for the different layers we allow the user to generate.
+ * Here we include type specifications for the JSON we receive over the wire, as well as the
+ * parameters for each individual node type.
+ */
+
+
+/**
+ * Top-level schema description for a node.
+ */
+export interface ModelSchema {
+    layers: ILayer[];
+    connections: IConnection[];
+}
+
+/**
+ * Serialized object format for a layer.
+ */
+export interface ILayer {
+    name: string;
+    kind: LayerKind;
+    params?: object;
+}
+
+/**
+ * Representation of a connection between two INode's.
+ */
+export interface IConnection {
+    head: string;
+    tail: string;
+}
+
+/**
+ * TypeAssertions is a group of methods for performing assertions on the "params" for each layer type to make sure they
+ * have all required properties. Currently it does NOT check the types on each of the members of the params.
+ */
+export class TypeAssertions {
+    static isInputParams(params: any): boolean {
+        return (typeof params === "object") && params.hasOwnProperty("dimensions");
+    }
+
+    static isFullyConnectedParams(params: any): boolean {
+        return (typeof params === "object") && params.hasOwnProperty("activation")
+                                            && (typeof params.activation === "string")
+                                            && params.hasOwnProperty("output_units")
+                                            && (typeof params.output_units === "number");
+    }
+
+    static isConv2DParams(params: any): boolean {
+        return (typeof params === "object") && params.hasOwnProperty("activation")
+                                            && (typeof params.activation === "string")
+                                            && params.hasOwnProperty("filters")
+                                            && (typeof params.filters === "number")
+                                            && params.hasOwnProperty("kernel_size")
+                                            && (typeof params.kernel_size === "object")
+                                            && params.hasOwnProperty("padding")
+                                            && (typeof params.padding === "string");
+    }
+
+    static isPool2DParams(params: any): boolean {
+        return (typeof params === "object") && params.hasOwnProperty("pool_size")
+                                            && (typeof params.pool_size === "object")
+                                            && params.hasOwnProperty("stride")
+                                            && (typeof params.stride === "number");
+    }
+
+    static isDropoutParams(params: any): boolean {
+        return (typeof params === "object") && params.hasOwnProperty("probability")
+                                            && (typeof params.probability === "number");
+    }
+
+    static isSoftmaxParams(params: any): boolean {
+        return (typeof params === "object") && params.hasOwnProperty("classes")
+                                            && (typeof params.classes === "number");
+    }
+}
 
 /**
  * Built-in layer types. All layers are a composition of thse 6 built-in types.
@@ -14,49 +90,31 @@ export type Activation = "sigmoid" | "tanh" | "relu";
  */
 export type Padding = "same" | "valid";
 
-export interface InputParams {
+export interface IInputParams {
     dimensions: [number];
 }
 
-export interface FullyConnectedParams {
+export interface IFullyConnectedParams {
     activation: Activation;
     output_units: number;
 }
 
-export interface Conv2DParams {
+export interface IConv2DParams {
+    activation: Activation;
     filters: number;
     kernel_size: [number];
     padding: Padding;
-    activation: Activation;
 }
 
-export interface Pool2DParams {
+export interface IPool2DParams {
     pool_size: [number];
-    strides: number;
+    stride: number;
 }
 
-export interface DropoutParams {
+export interface IDropoutParams {
     probability: number;
 }
 
-export interface SoftmaxParams {
+export interface ISoftmaxParams {
     classes: number;
-}
-
-/**
- * Node in a raw graph that needs to be compiled.
- */
-export interface INode {
-    name: string;
-    kind: LayerKind;
-    params?: object;
-}
-
-/**
- * An IGraph is the JSON interface we expect to receive over the wire in a request to compile.
- * Nodes are a collection of INode JSON objects, and edges is a list of
- */
-export interface IGraph {
-    nodes: INode[];
-    edges: {[key: string]: string};
 }
