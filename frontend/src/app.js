61c28f37bb65016533ecdb12bf3c9ab58fde9a4d
Todd Macdonald <tmacd@stanford.edu>
2017-05-12 01:35:55 -0700

    added dragging functionality of neural network editor. can easily add to this input boxes, ect

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    angular.module('axonApp', ['ngRoute', 'ngResource', 'ngMaterial', 'ui.ace', 'btford.markdown']);
+    angular.module('axonApp', ['ngRoute', 'ngResource', 'ngMaterial', 'ui.ace', 'btford.markdown', 'dndLists']);
 
 })();
 
