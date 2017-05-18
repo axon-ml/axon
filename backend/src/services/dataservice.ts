0586439d5515c3210572181174bc9cdc4bb729c3
Andrew Duffy <andreweduffy@gmail.com>
2017-05-18 12:28:20 -0700

    Added reverse lookup routes for userId and modelId

--- backend/src/services/dataservice.ts
+++ backend/src/services/dataservice.ts
@@ -1,8 +1,10 @@
-import {Request, Response, Router} from "express";
+import {HttpCodes} from "../httpcodes";
 import {contentType} from "../middleware";
 import {Service} from "./service";
-import * as pg from "pg";
 import {createLogger} from "../logger";
+
+import {Request, Response, Router} from "express";
+import * as pg from "pg";
 
 const LOGGER = createLogger("DataService");
 
@@ -20,7 +22,9 @@ export class DataService extends Service {
 
     protected setupRoutes(): Router {
         return Router()
-            .get("/models/:username", (req, res) => this.handleModels(req, res));
+            .get("/models/:username", (req, res) => this.handleModels(req, res))
+            .get("/id/:username", (req, res) => this.handleReverseLookupUserId(req, res))
+            .get("/id/:username/:modelname", (req, res) => this.handleReverseLookupModelId(req, res));
     }
 
     private handleModels(req: Request, res: Response) {
@@ -32,15 +36,60 @@ export class DataService extends Service {
         this.db.query(query, [username], (err, result) => {
             if (err) {
                 LOGGER.error(`Postgres error: ${err}`);
-                res.status(500).send(err);
-            } else {
-                const models = [];
-                for (const row of result.rows) {
-                    models.push(row);
-                }
-                LOGGER.info(`Models for ${username}: ${JSON.stringify(models)}`);
-                res.json({models: models}).end();
+                return res.status(HttpCodes.INTERNAL_SERVER_ERROR).send(err);
             }
+
+            const models = [];
+            for (const row of result.rows) {
+                models.push(row);
+            }
+            LOGGER.info(`Models for ${username}: ${JSON.stringify(models)}`);
+            return res.json({models: models}).end();
+        });
+    }
+
+    private handleReverseLookupUserId(req: Request, res: Response) {
+        const query = `
+        SELECT id
+        FROM users
+        WHERE handle = $1
+        `;
+        this.db.query(query, [req.params.username], (err, results) => {
+            if (err) {
+                LOGGER.error(`Postgres error: ${err}`);
+                return res.status(HttpCodes.INTERNAL_SERVER_ERROR).send(err);
+            }
+
+            if (results.rowCount === 0) {
+                LOGGER.error(`No such user ${req.params.username}`);
+                return res.status(HttpCodes.NOT_FOUND).send(`No such user ${req.params.username}`);
+            }
+
+            // Return the user id
+            return res.json({id: results.rows[0].id});
+        });
+    }
+
+    private handleReverseLookupModelId(req: Request, res: Response) {
+        // Search for the model based on the username and modelname combo
+        const query = `
+        SELECT models.id as id
+        FROM models, users
+        WHERE users.handle = $1 AND models.name = $2 AND models.owner = users.id
+        `;
+        this.db.query(query, [req.params.username, req.params.modelname], (err, results) => {
+            if (err) {
+                LOGGER.error(`Postgres error: ${err}`);
+                return res.status(HttpCodes.INTERNAL_SERVER_ERROR).send(err);
+            }
+
+            if (results.rowCount === 0) {
+                const error = `No such model ${req.params.username}/${req.params.modelname}`;
+                LOGGER.error(error);
+                return res.status(HttpCodes.NOT_FOUND).send(error);
+            }
+
+            return res.json({id: results.rows[0].id});
         });
     }
 }
