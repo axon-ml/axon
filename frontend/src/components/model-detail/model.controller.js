c6e763e1d4df8a8234ca8e35eea0ef46c7d21ac1
Andrew Duffy <andreweduffy@gmail.com>
2017-06-11 16:41:46 -0700

    Only allow editing of markdown + model by owner.

--- frontend/src/components/model-detail/model.controller.js
+++ frontend/src/components/model-detail/model.controller.js
@@ -17,8 +17,8 @@
         vm.star = star;
         vm.starCount = undefined;
 
-        // Only display editor tab for logged-in user's models.
-        vm.displayEditor = vm.username === $rootScope.root.username;
+        // Only make components editable for logged-in user's models.
+        vm.editable = vm.username === $rootScope.root.username;
 
         if (vm.markdown) {
             vm.leftNav = 'preview';
